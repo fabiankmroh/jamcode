5f8838ffaff4ac7fcf084d5cdc80b00fde032f79
Fabian Roh <fabiankmroh@gmail.com>
2019-02-25 18:43:49 +0900

    20190225 Session

---
+++ practice/2001.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+// LIS algorithm
+
+int main(void)
---
+++ practice/5557.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int num[100];
+int sign[100];
+int lim;
+int final;
+int i;
+
+int
+
+// how to put signs in each branch? Two dimensional array?
+
+int main(void){
+    scanf("%d", &lim);
+
+    for(i = 0; i < lim-1; i++){
+        scanf("%d", &num[i]);
+    }
+    final = num[i+1];
+
+
+}
