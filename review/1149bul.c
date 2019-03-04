340e0f2b24af013ec6472fb65d02404282bb5e80
Fabian Roh <fabiankmroh@gmail.com>
2019-03-04 17:16:02 +0900

    Second Attempt.FAILED

--- review/1149bul.c
+++ review/1149bul.c
@@ -1,59 +1,56 @@
 // FAILED
 #include <stdio.h>
 
-int cost[3][1500];
-int ans[3][1500];
+int cost[1500][3];
+int ans[1500][3];
 int lim;
 int i, j;
+int ii, jj;
 int final = 999;
 
 int main(void){
     scanf("%d", &lim);
     for(i = 0; i < lim; i++){
         for(j = 0; j < 3; j++){
-            scanf("%d", &cost[j][i]);
+            scanf("%d", &cost[i][j]);
         }
+    }
+
+    for(i = 0; i < 3; i++){
+        ans[lim-1][i] = cost[lim-1][i];
     }
 
     for(i = lim-2; i >= 0; i--){
         for(j = 0; j < 3; j++){
             if(j == 0){
-                if(cost[1][i+1] < cost[2][i+1]){
-                    ans[j][i] = cost[j][i] + cost[1][i+1];
+                if(ans[i+1][1] < ans[i+1][2]){
+                    ans[i][j] = cost[i][j] + ans[i+1][1];
                 }
-                else if(cost[1][i+1] > cost[2][i+1]){
-                    ans[j][i] = cost[j][i] + cost[2][i+1];
+                else{
+                    ans[i][j] = cost[i][j] + ans[i+1][2];
                 }
             }
             else if(j == 1){
-                if(cost[0][i+1] < cost[2][i+1]){
-                    ans[j][i] = cost[j][i] + cost[0][i+1];
+                if(ans[i+1][0] < ans[i+1][2]){
+                    ans[i][j] = cost[i][j] + ans[i+1][0];
                 }
-                else if(cost[0][i+1] > cost[2][i+1]){
-                    ans[j][i] = cost[j][i] + cost[2][i+1];
+                else{
+                    ans[i][j] = cost[i][j] + ans[i+1][2];
                 }
             }
             else if(j == 2){
-                if(cost[0][i+1] < cost[2][i+1]){
-                    ans[j][i] = cost[j][i] + cost[0][i+1];
+                if(ans[i+1][0] < ans[i+1][1]){
+                    ans[i][j] = cost[i][j] + ans[i+1][0];
                 }
-                else if(cost[0][i+1] > cost[2][i+1]){
-                    ans[j][i] = cost[j][i] + cost[2][i+1];
+                else{
+                    ans[i][j] = cost[i][j] + ans[i+1][1];
                 }
-            }
-            
-            printf("%d %d\n", i, j);
-            for(i = 0; i < lim; i++){
-                for(j = 0; j < 3; j++){
-                    printf("%d ", ans[i][j]);
-                }
-                printf("\n");
             }
         }
     }
 
     for(i = 0; i < 3; i++){
-        if(ans[i][0] < final) final = ans[i][0];
+        if(ans[0][i] < final) final = ans[0][i];
     }
 
     printf("%d\n", final);
