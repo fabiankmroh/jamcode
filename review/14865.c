b14faedc36ddd40f2e273a1308e9135283eebd4e
Fabian Roh <fabiankmroh@gmail.com>
2019-04-01 17:30:48 +0900

    14685 TIME OUT

--- review/14865.c
+++ review/14865.c
@@ -1,7 +1,24 @@
+/* Hand Draw Successful. TIMED OUT.
+
+#1 Directly '0' or '1' on plane coordinate
+#2 Store vertice coordinates in another array (w/o plane coordinate)
+and determine hill status using if else cases */
+
 #include <stdio.h>
+int plane[123456][123456];
 
 int main(void){
-    int coordiante[2][1500000000];
+    int vertices;
+    int x, y;
+    int i, j;
 
-    
+    scanf("%d", &vertices);
+
+    for(i = 0; i < vertices; i++){
+        scanf("%d %d", &x, &y);
+
+        plane[x][y] = 1;
+    }
+
+    for(i)
 }
