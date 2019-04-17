0882e2828bcf9089541b1631900225fcf7d78aa3
Fabian Roh <fabiankmroh@gmail.com>
2019-04-17 18:38:56 +0900

    ld & rd accumulation dilemma

--- arrow.c
+++ arrow.c
@@ -1,45 +1,50 @@
 #include <stdio.h>
 
 int main(void){
-    int lim;
-    int loc[123456789]; // Coordinate Location List
-    int col[123456789]; // Color List (Matched Accordingly)
+    int loc[4] = {0,1,3,4}; // Coordinate Location List
+    int col[4] = {1,2,1,1}; // Color List (Matched Accordingly)
+    int lim = 4;
     int i, j; 
     int currLoc;
     int ld, rd; // left distance, right distance
     int final = 0;
 
-    scanf("%d", &lim);
+    /* scanf("%d", &lim);
     
     for(i = 0; i <= lim; i++){
         scanf("%d", &loc[i]);
         scanf("%d", &col[i]);
-    }
+    } */
 
-    for(i = 0; i <= lim; i++){
-        loc[i] = currLoc;
+    for(i = 0; i < lim; i++){
+        ld = 0;
+        rd = 0;
 
         // Going Left
-        for(j = currLoc; j >= 0; j--){
-            if(col[j] == col[i]){
-                ld = i - j;
-                break;
+        for(j = i-1; j >= 0; j--){
+            if(loc[j] != loc[i]){
+                if(col[j] == col[i]){
+                    ld = loc[i] - loc[j];
+                    break;
+                }
             }
         }
 
         // Going Right
-        for(j = currLoc; j <= lim; j++){
-            if(col[j] == col[i]){
-                rd = j - i;
-                break;
+        for(j = i+1; j < lim; j++){
+            if(loc[j] != loc[i]){
+                if(col[j] == col[i]){
+                    rd = loc[j] - loc[i];
+                    break;
+                }
             }
         }
 
         // Distance Comparison
-        if(ld > rd){
+        if(ld < rd && ld > 0){
             final += ld;
         }
-        else{
+        else if(ld > rd && rd > 0){
             final += rd;
         }
     }
